8a0db457bf9f339f1f3802c7e932b0e87cf4dc21
riskiardian <99702053+kiidspyro@users.noreply.github.com>
2024-02-07 08:10:44 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Zygisk-test
+# Zygisk Next
+
+Standalone implementation of Zygisk, providing Zygisk API support for KernelSU and a replacement of Magisk's built-in Zygisk.
+
+## Requirements
+
+### General
+
++ No multiple root implementation installed
+
+### KernelSU
+
++ Minimal KernelSU version: 10940
++ Minimal KernelSU Manager (ksud) version: 11424
++ Kernel has full SELinux patch support
+
+### Magisk
+
++ Minimal version: 26402
++ Built-in Zygisk turned off
+
+## Compatibility
+
+`PROCESS_ON_DENYLIST` cannot be flagged correctly for isolated processes on Magisk DenyList currently.
+
+Zygisk Next only guarantees the same behavior of Zygisk API, but will NOT ensure Magisk's internal features.
---
+++ build.gradle.kts
@@ -0,0 +1,65 @@
+import com.android.build.gradle.LibraryExtension
+import java.io.ByteArrayOutputStream
+
+plugins {
+    alias(libs.plugins.agp.lib) apply false
+}
+
+fun String.execute(currentWorkingDir: File = file("./")): String {
+    val byteOut = ByteArrayOutputStream()
+    project.exec {
+        workingDir = currentWorkingDir
+        commandLine = split("\\s".toRegex())
+        standardOutput = byteOut
+    }
+    return String(byteOut.toByteArray()).trim()
+}
+
+val gitCommitCount = "git rev-list HEAD --count".execute().toInt()
+val gitCommitHash = "git rev-parse --verify --short HEAD".execute()
+
+val moduleId by extra("zygisksu")
+val moduleName by extra("Zygisk Next")
+val verName by extra("v4-0.9.1.1")
+val verCode by extra(gitCommitCount)
+val commitHash by extra(gitCommitHash)
+val minKsuVersion by extra(10940)
+val minKsudVersion by extra(11425)
+val maxKsuVersion by extra(20000)
+val minMagiskVersion by extra(26402)
+
+val androidMinSdkVersion by extra(26)
+val androidTargetSdkVersion by extra(34)
+val androidCompileSdkVersion by extra(34)
+val androidBuildToolsVersion by extra("34.0.0")
+val androidCompileNdkVersion by extra("26.0.10792818")
+val androidSourceCompatibility by extra(JavaVersion.VERSION_11)
+val androidTargetCompatibility by extra(JavaVersion.VERSION_11)
+
+tasks.register("Delete", Delete::class) {
+    delete(rootProject.buildDir)
+}
+
+fun Project.configureBaseExtension() {
+    extensions.findByType(LibraryExtension::class)?.run {
+        namespace = "icu.nullptr.zygisk.next"
+        compileSdk = androidCompileSdkVersion
+        ndkVersion = androidCompileNdkVersion
+        buildToolsVersion = androidBuildToolsVersion
+
+        defaultConfig {
+            minSdk = androidMinSdkVersion
+            targetSdk = androidTargetSdkVersion
+        }
+
+        lint {
+            abortOnError = true
+        }
+    }
+}
+
+subprojects {
+    plugins.withId("com.android.library") {
+        configureBaseExtension()
+    }
+}
